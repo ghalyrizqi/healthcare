851c1856ea8f115efdb15013662953b882d35e20
rizqirr <rizqirr@gmail.com>
2024-09-17 15:29:43 +0000

    sementara

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-white text-3xl underline">Home</h1>
-      <Button>Click me</Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image></Image>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
